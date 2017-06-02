9edc48e33bbe22162e0136b53fb3e5d48fa8f03d
riverajefer <jefersonpatino@yahoo.es>
2017-06-02 16:23:08 -0500

    servedr

--- main.js
+++ main.js
@@ -1,27 +1,79 @@
-console.log("Hello Word");
-
 var app = new Vue({
     el: "#app" ,
     data:{
-        tasks:[
-            {id: "ch1", name:'tarea 1', done:true,  order:1},
-            {id: "ch2", name:'tarea 2', done:false, order:2},
-            {id: "ch3", name:'tarea 3', done:true,  order:3},
-            {id: "ch4", name:'tarea 4', done:true,  order:4},
-         ]
+        titulo:'',
+        tasks:[],
+        errors:[]
     },
     methods:{
         addTask: function(){
-            this.tasks.unshift({
-                id:"ch6"+Math.random(),
-                name:this.tasks.name,
-                done:false
+            axios.post('http://192.168.0.10/laravelPruebas/public/add_task', {task:this.tasks.task})
+              .then(response => {
+                  console.log("response save: ",response.data)
+                  this.tasks.unshift(response.data)
+                  this.tasks.task=""
+              })
+              .catch(e => {
+                 this.errors.push(e)
+               })
+        },
+        done(task){
+            axios.post('http://192.168.0.10/laravelPruebas/public/done_task', {task:task})
+            .then(response => {
+                console.log("response done: ",response.data)
             })
-            this.tasks.name=""
+            .catch(e => {
+                this.errors.push(e)
+            })            
+            
         },
         eliminar: function(task){
-            this.tasks.splice(this.tasks.indexOf(task), 1)
+            axios.post('http://192.168.0.10/laravelPruebas/public/delete_task', {task:task.id})
+            .then(response => {
+                console.log("response done: ",response.data)
+                this.tasks.splice(this.tasks.indexOf(task), 1)
+            })
+            .catch(e => {
+                this.errors.push(e)
+            })  
+        },
+        checkMove: function(evt){
+            var task = evt.draggedContext.element
+            var futureIndex = evt.draggedContext.futureIndex
+            console.log("current task: ", task)
+            axios.post('http://192.168.0.10/laravelPruebas/public/order_task', {task:task.id,futureIndex:futureIndex})
+            .then(response => {
+                console.log("response move: ",response.data)
+            })
+            .catch(e => {
+                this.errors.push(e)
+            })              
+        },
+        editar: function(task){
+            console.log("eidtar: ", task);
+            this.titulo = task.task
+            $('#modal1').modal('open');
+
         }
-       
-    }
+     },
+    mounted(){
+        $('#modal1').modal('open');
+
+
+        axios.get(`http://192.168.0.10/laravelPruebas/public/tasks`)
+        .then(response => {
+            this.tasks = response.data
+        })
+        .catch(e => {
+            this.errors.push(e)
+        })
+
+        console.log("lista: ", this.tasks)
+    },
+})
+
+
+$(document).ready(function(){
+// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+$('.modal').modal();
 })
